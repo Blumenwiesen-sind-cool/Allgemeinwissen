c43e9db6ecd28cabc7ac21fff756f760861a6f59
B0rder-L1ne <paulinemaile@maile-ko.de>
2024-06-17 12:32:10 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,171 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Wie heißt die Haupstadt der Bundesrepublik Deutschland?",
+    answer: "Berlin",
+    options: [
+      "Bonn",
+      "Berlin",
+      "Frankfurt",
+      "München"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Wie heißt die Landeshauptstadt von Hessen?",
+    answer: "Wiesbaden",
+    options: [
+      "Wiesbaden",
+      "Frankfurt",
+      "Darmstadt",
+      "Ludwigsburg"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Wie viel Bundesländer hat Deutschland?",
+    answer: "16",
+    options: [
+      "18",
+      "15",
+      "14",
+      "16"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Wie heißt die Hauptstadt von Lettland?",
+    answer: "Riga",
+    options: [
+      "Riga",
+      "Budapest",
+      "Helsinki",
+      "Oslo"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Wie viele Beine hat eine Spinne?",
+    answer: "8",
+    options: [
+      "6",
+      "8",
+      "10",
+      "4"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Wie viele Farben hat ein Regenbogen?",
+    answer: "7",
+    options: [
+      "7",
+      "6",
+      "8",
+      "5"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Welches Organ ist bei einem Menschen am größten?",
+    answer: "Haut",
+    options: [
+      "Darm",
+      "Leber",
+      "Das ist bei jedem Menschen unterschiedlich",
+      "Haut"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Spaß Frage für Zwischendurch: Können Nilpferde schwimmen?",
+    answer: "Nein, ihr Körper hat eine so hohe Dichte, dass sie auf dem Grund des Gewässers laufen können",
+    options: [
+      "Nein, sie haben einen so hohen Fettanteil, dass sie auf dem Wasser treiben",
+      "Sie können nur im Nil schwimmen, deshalb heißen sie so",
+      "Nein, ihr Körper hat eine so hohe Dichte, dass sie auf dem Grund des Gewässers laufen können",
+      "Ja, natürlich können sie schwimmen!"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Und noch eine! Was können Elefanten NICHT?",
+    answer: "Springen",
+    options: [
+      "Malen",
+      "Vögel imitieren",
+      "Ihre Artgenossen vor Bienen warnen",
+      "Springen"
+    ]
+  },
+  // Uncomment the below questions to add more
+  /*
+  {
+    numb: 10,
+    question: "Question 10",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Question 11",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Question 12",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Question 13",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Question 14",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Question 15",
+    answer: "-",
+    options: [
+      "a",
+      "b",
+      "c",
+      "d"
+    ]
+  }
+  */
+];
